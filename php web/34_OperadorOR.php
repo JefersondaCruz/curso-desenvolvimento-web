9254dc25302651e1d78495a4f4fb69fde66e5b73
Jeferson da Cruz <jefersondacruz40@gmail.com>
2024-08-30 16:47:08 -0300

    newcontent/php

--- php web/34_OperadorOR.php
+++ php web/34_OperadorOR.php
@@ -14,3 +14,20 @@
     if (5 > 2 || 3 < 4) { // true e true 
         echo "A operação 4 é verdadeira \n ";
     }
+
+    $a = 10;
+    $b = 20;
+    $c = 30;
+    $d = 40;
+
+    if ($a > $b || $d > $c ) {
+        echo "A operação 5 é verdadeira \n";
+    }
+
+    if (($a > $b || $d > $c) && $c < $d ){
+        echo "A operação 6 é verdadeira \n";
+    }
+
+    if (($a > $b || $d > $c) || $c < $d ){
+        echo "A operação 7 é verdadeira \n";
+    }
