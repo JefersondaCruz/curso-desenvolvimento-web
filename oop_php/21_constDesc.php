ea3ce39121f516a6d2cbb91ea3d9439f85fbd8f3
JefersondaCruz <Jefersondacruz40@gmail.com>
2024-09-10 16:59:55 -0300

    NewContent

--- oop_php/21_constDesc.php
+++ oop_php/21_constDesc.php
@@ -11,9 +11,29 @@
         }
 
         function __destruct() {
-            
+            echo "E morreu! \n";
         
         }
+
+        public function apresentar() {
+            echo "{$this->nome}, {$this->idade} anos \n";
+        }
     }
+
+     // $pessoa = new Pessoa(); // problema, precisa passar os valores entre aspas
+    
+
+    $pessoaA = new Pessoa("Rebeca Maria", 40);
+    $pessoaA->apresentar();
+    unset($pessoaA);
+
+    $pessoaB = new Pessoa("Joao silva", 60);
+    $pessoaB->apresentar();
+    $pessoaB = null;
+
+
+    
+    
+    
 
     
