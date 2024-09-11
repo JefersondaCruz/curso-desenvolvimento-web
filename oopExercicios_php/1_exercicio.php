e8d249875cceef7a6631717edbed956a844b6941
Jeferson da Cruz <jefersondacruz40@gmail.com>
2024-09-10 23:43:48 -0300

    newcontent

---
+++ oopExercicios_php/1_exercicio.php
@@ -0,0 +1,42 @@
+<?php 
+
+    class Pessoa {
+        public $Nome_Pessoa;
+        public $idade;
+        public $dia_nascimento;
+        public $mes_nascimento;
+        public $ano_nascimento;
+
+        public function calculaIdade() {
+            $this->dia_nascimento;
+            $this->mes_nascimento;
+            $this->ano_nascimento;
+            return $this->ano_nascimento - 2024 . "anos \n";
+            
+        }
+
+        public function informarIdade() {
+            return $this->calculaIdade();
+        }
+
+        public function informaNome() {
+            $this->Nome_Pessoa;
+            return $this->Nome_Pessoa;
+
+        }
+
+        public function ajustaDataDeNascimento () {
+            $this->dia_nascimento;
+            $this->mes_nascimento;
+            $this->ano_nascimento;
+
+            return "{$this->dia_nascimento}/{$this->mes_nascimento}/{$this->ano_nascimento} \n"; 
+        }
+
+    }
+
+        $pessoa1 = new Pessoa;
+        $pessoa1->Nome_Pessoa = readline("Digite o nome da pessoa: ");
+        $pessoa1->dia_nascimento = readline("Digite o seu dia de nascimento: ");
+        $pessoa1->mes_nascimento = readline("Digite o seu mes de nascimento");
+        $pessoa1->ano_nascimento = readline("Digite seu ano de nascimento: ");
